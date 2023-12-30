323986851663eb97f6a5fce8e1f7028e48f5e0e5
Jovan Gerodetti <jovan.gerodetti@titannano.de>
2023-12-31 00:11:49 +0100

    Remove hot reloader (#18)

--- rust-script/src/runtime/mod.rs
+++ rust-script/src/runtime/mod.rs
@@ -1,5 +1,3 @@
-#[cfg(all(feature = "hot-reload", debug_assertions))]
-mod hot_reloader;
 mod metadata;
 mod resource_loader;
 mod resource_saver;
@@ -9,7 +7,6 @@ mod rust_script_language;
 
 use std::{collections::HashMap, rc::Rc, sync::RwLock};
 
-use cfg_if::cfg_if;
 use godot::{
     engine::{Engine, ResourceLoader, ResourceSaver},
     prelude::{godot_print, Gd},
@@ -25,35 +22,6 @@ use crate::{
 
 use self::rust_script_language::RustScriptLanguage;
 
-#[cfg(all(feature = "hot-reload", debug_assertions))]
-use hot_reloader::{HotReloadEntry, HotReloader};
-
-#[cfg(all(feature = "hot-reload", debug_assertions))]
-#[macro_export]
-macro_rules! setup {
-    ($lib_crate:tt) => {
-        #[$crate::private_export::hot_module(dylib = stringify!($lib_crate), lib_dir=process_path::get_dylib_path().and_then(|path| path.parent().map(std::path::Path::to_path_buf)).unwrap_or_default())]
-        mod scripts_lib {
-            use $crate::private_export::RVec;
-
-            // manually expose functions.
-            #[hot_functions]
-            extern "Rust" {
-                pub fn __godot_rust_script_init(
-                    binding: Option<$crate::private_export::BindingInit>,
-                ) -> RVec<$crate::RemoteScriptMetaData>;
-            }
-
-            // expose a type to subscribe to lib load events
-            #[lib_change_subscription]
-            pub fn subscribe() -> hot_lib_reloader::LibReloadObserver {}
-
-            pub use ::$lib_crate::__GODOT_RUST_SCRIPT_SRC_ROOT;
-        }
-    };
-}
-
-#[cfg(not(all(feature = "hot-reload", debug_assertions)))]
 #[macro_export]
 macro_rules! setup {
     ($lib_crate:tt) => {
@@ -63,39 +31,18 @@ macro_rules! setup {
     };
 }
 
-#[cfg(not(all(feature = "hot-reload", debug_assertions)))]
 #[macro_export]
 macro_rules! init {
     () => {
         $crate::RustScriptExtensionLayer::new(
             scripts_lib::__godot_rust_script_init,
             scripts_lib::__GODOT_RUST_SCRIPT_SRC_ROOT,
-        )
-    };
-}
-
-#[cfg(all(feature = "hot-reload", debug_assertions))]
-#[macro_export]
-macro_rules! init {
-    () => {
-        $crate::RustScriptExtensionLayer::new(
-            scripts_lib::__godot_rust_script_init,
-            scripts_lib::__GODOT_RUST_SCRIPT_SRC_ROOT,
-            scripts_lib::subscribe,
         )
     };
 }
 
 thread_local! {
     static SCRIPT_REGISTRY: RwLock<HashMap<String, ScriptMetaData>> = RwLock::default();
-    #[cfg(all(feature = "hot-reload", debug_assertions))]
-    static HOT_RELOAD_BRIDGE: std::cell::RefCell<HashMap<rust_script_instance::RustScriptInstanceId, std::cell::RefCell<HotReloadEntry>>> = std::cell::RefCell::default();
-}
-
-cfg_if! {
-    if #[cfg(all(feature = "hot-reload", debug_assertions))] {
-        type HotReloadSubscribe = fn() -> hot_lib_reloader::LibReloadObserver;
-    }
 }
 
 pub struct RustScriptExtensionLayer {
@@ -104,20 +51,12 @@ pub struct RustScriptExtensionLayer {
     res_saver: Option<Gd<RustScriptResourceSaver>>,
     res_loader: Option<Gd<RustScriptResourceLoader>>,
     scripts_src_dir: Option<&'static str>,
-
-    #[cfg(all(feature = "hot-reload", debug_assertions))]
-    hot_reload_subscribe: HotReloadSubscribe,
-
-    #[cfg(all(feature = "hot-reload", debug_assertions))]
-    hot_reloader: Option<Gd<HotReloader>>,
 }
 
 impl RustScriptExtensionLayer {
     pub fn new<F: RustScriptLibInit + 'static + Clone>(
         lib_init_fn: F,
         scripts_src_dir: &'static str,
-        #[cfg(all(feature = "hot-reload", debug_assertions))]
-        hot_reload_subscribe: HotReloadSubscribe,
     ) -> Self {
         Self {
             lib_init_fn: Rc::new(lib_init_fn),
@@ -125,12 +64,6 @@ impl RustScriptExtensionLayer {
             res_saver: None,
             res_loader: None,
             scripts_src_dir: Some(scripts_src_dir),
-
-            #[cfg(all(feature = "hot-reload", debug_assertions))]
-            hot_reload_subscribe,
-
-            #[cfg(all(feature = "hot-reload", debug_assertions))]
-            hot_reloader: None,
         }
     }
 
@@ -140,18 +73,6 @@ impl RustScriptExtensionLayer {
         let lang: Gd<RustScriptLanguage> = RustScriptLanguage::new(self.scripts_src_dir);
         let res_loader = RustScriptResourceLoader::new(lang.clone());
         let res_saver = Gd::from_object(RustScriptResourceSaver);
-
-        cfg_if! {
-            if #[cfg(all(feature = "hot-reload", debug_assertions))] {
-                use godot::prelude::StringName;
-
-                let mut hot_reloader = Gd::from_init_fn(|base| HotReloader::new((self.hot_reload_subscribe)(), self.lib_init_fn.clone(), base));
-
-                hot_reloader.call_deferred(StringName::from("register"), &[]);
-
-                self.hot_reloader = Some(hot_reloader);
-            }
-        }
 
         self.lang = Some(lang.clone());
         self.res_saver = Some(res_saver.clone());
@@ -186,15 +107,7 @@ impl RustScriptExtensionLayer {
 }
 
 fn load_rust_scripts(lib_init_fn: Rc<dyn RustScriptLibInit>) {
-    cfg_if! {
-        if #[cfg(all(feature = "hot-reload", debug_assertions))] {
-            let ffi_init = Some(unsafe { godot::sys::get_binding() });
-        } else {
-            let ffi_init = None;
-        }
-    }
-
-    let result = lib_init_fn(ffi_init);
+    let result = lib_init_fn();
 
     let registry: HashMap<String, ScriptMetaData> = result
         .into_iter()
--- rust-script/src/runtime/rust_script_instance.rs
+++ rust-script/src/runtime/rust_script_instance.rs
@@ -1,13 +1,6 @@
 use std::{collections::HashMap, rc::Rc};
 
-#[cfg(all(feature = "hot-reload", debug_assertions))]
-use std::{
-    cell::RefCell,
-    ops::{Deref, DerefMut},
-};
-
 use abi_stable::std_types::{RBox, RString};
-use cfg_if::cfg_if;
 use godot::{
     builtin::ScriptInstance,
     engine::Script,
@@ -24,13 +17,7 @@ use crate::{
     script_registry::{RemoteGodotScript_TO, RemoteValueRef},
 };
 
-#[cfg(all(feature = "hot-reload", debug_assertions))]
-use crate::runtime::hot_reloader::HotReloadEntry;
-
 use super::{rust_script::RustScript, rust_script_language::RustScriptLanguage, SCRIPT_REGISTRY};
-
-#[cfg(all(feature = "hot-reload", debug_assertions))]
-use super::HOT_RELOAD_BRIDGE;
 
 fn script_method_list(script: &Gd<RustScript>) -> Rc<Vec<MethodInfo>> {
     let rs = script.bind();
@@ -63,25 +50,10 @@ fn script_property_list(script: &Gd<RustScript>) -> Rc<Vec<PropertyInfo>> {
 }
 
 pub(super) struct RustScriptInstance {
-    #[cfg(not(all(feature = "hot-reload", debug_assertions)))]
     data: RemoteGodotScript_TO<'static, RBox<()>>,
-
-    #[cfg(all(feature = "hot-reload", debug_assertions))]
-    data: RustScriptInstanceId,
 
     gd_object: Gd<Object>,
     script: Gd<RustScript>,
-}
-
-#[cfg(all(feature = "hot-reload", debug_assertions))]
-#[derive(Hash, PartialEq, Eq, Clone, Copy)]
-pub(super) struct RustScriptInstanceId(usize);
-
-#[cfg(all(feature = "hot-reload", debug_assertions))]
-impl RustScriptInstanceId {
-    pub fn new() -> Self {
-        Self(rand::random())
-    }
 }
 
 impl RustScriptInstance {
@@ -90,83 +62,24 @@ impl RustScriptInstance {
         gd_object: Gd<Object>,
         script: Gd<RustScript>,
     ) -> Self {
-        cfg_if! {
-            if #[cfg(all(feature = "hot-reload", debug_assertions))] {
-                let id = RustScriptInstanceId::new();
-
-                HOT_RELOAD_BRIDGE.with(|map| {
-                    let entry = HotReloadEntry::new(
-                        data,
-                        script.clone(),
-                        gd_object.clone(),
-                    );
-
-                    map.borrow_mut().insert(id, RefCell::new(entry))
-                });
-
-                Self {
-                    data: id,
-                    gd_object,
-                    script,
-                }
-            } else {
-                Self {
-                    data,
-                    gd_object,
-                    script,
-                }
-            }
+        Self {
+            data,
+            gd_object,
+            script,
         }
     }
 }
 
-cfg_if! {
-    if #[cfg(all(feature = "hot-reload", debug_assertions))] {
-        impl RustScriptInstance {
-            fn with_data<T>(&self, cb: impl FnOnce(&RemoteGodotScript_TO<'static, RBox<()>>) -> T) -> T {
-                HOT_RELOAD_BRIDGE.with(|map| {
-                    let map_ref = map.borrow();
+impl RustScriptInstance {
+    fn with_data<T>(&self, cb: impl FnOnce(&RemoteGodotScript_TO<'static, RBox<()>>) -> T) -> T {
+        cb(&self.data)
+    }
 
-                    let inst = map_ref
-                        .get(&self.data)
-                        .expect("not having the remote script instance is fatal");
-
-                    let result = cb(&inst.borrow().deref().instance);
-
-                    result
-                })
-            }
-
-            fn with_data_mut<T>(
-                &self,
-                cb: impl FnOnce(&mut RemoteGodotScript_TO<'static, RBox<()>>) -> T,
-            ) -> T {
-                HOT_RELOAD_BRIDGE.with(|map| {
-                    let map_ref = map.borrow();
-
-                    let inst = map_ref
-                        .get(&self.data)
-                        .expect("not having the remote script instance is fatal");
-
-                    let result = cb(&mut inst.borrow_mut().deref_mut().instance);
-
-                    result
-                })
-            }
-        }
-    } else {
-        impl RustScriptInstance {
-            fn with_data<T>(&self, cb: impl FnOnce(&RemoteGodotScript_TO<'static, RBox<()>>) -> T) -> T {
-                cb(&self.data)
-            }
-
-            fn with_data_mut<T>(
-                &mut self,
-                cb: impl FnOnce(&mut RemoteGodotScript_TO<'static, RBox<()>>) -> T,
-            ) -> T {
-                cb(&mut self.data)
-            }
-        }
+    fn with_data_mut<T>(
+        &mut self,
+        cb: impl FnOnce(&mut RemoteGodotScript_TO<'static, RBox<()>>) -> T,
+    ) -> T {
+        cb(&mut self.data)
     }
 }
 
