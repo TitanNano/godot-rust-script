1f734dfec7b72b30d1cb88e7947749b3a99ec809
Jovan Gerodetti <jovan.gerodetti@titannano.de>
2024-01-01 22:48:02 +0100

    Add LICENSE (#19)

--- rust-script/src/runtime/rust_script.rs
+++ rust-script/src/runtime/rust_script.rs
@@ -1,3 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 use std::{cell::RefCell, collections::HashSet, ffi::c_void, ops::Deref};
 
 use abi_stable::std_types::RBox;
