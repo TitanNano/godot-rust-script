5bc8ed191d6a0ba0683d380b89a4d4cfe3df44b0
Jovan Gerodetti <jovan.gerodetti@titannano.de>
2024-01-08 01:04:03 +0100

    Upgrade gdext

---
+++ rust-script/src/runtime/downgrade_self.rs
@@ -0,0 +1,38 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+use std::ops::Deref;
+
+use godot::{
+    builtin::meta::ToGodot,
+    obj::{Gd, GodotClass, Inherits, WithBaseField},
+};
+
+pub trait DowngradeSelf: GodotClass {
+    fn downgrade<F: FnOnce(&Self) -> R, R>(&mut self, closure: F) -> R;
+    fn downgrade_gd<F: FnOnce(Gd<Self>) -> R, R>(&mut self, closure: F) -> R;
+}
+
+impl<T> DowngradeSelf for T
+where
+    T: WithBaseField + GodotClass,
+    T: Inherits<<T as GodotClass>::Base>,
+{
+    fn downgrade<F: FnOnce(&Self) -> R, R>(&mut self, closure: F) -> R {
+        let mut_base = self.base_mut();
+        let self_gd: Gd<T> = mut_base.to_godot().cast();
+        let slf = self_gd.bind();
+
+        closure(slf.deref())
+    }
+
+    fn downgrade_gd<F: FnOnce(Gd<Self>) -> R, R>(&mut self, closure: F) -> R {
+        let mut_base = self.base_mut();
+        let self_gd = mut_base.to_godot().cast();
+
+        closure(self_gd)
+    }
+}
